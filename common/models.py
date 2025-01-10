80558c1e94bc3728aa22255786b32179590a8e77
Shoxruh Xolboyev <kholboevshohrukh@gmail.com>
2025-01-10 13:48:37 +0500

    Add models

--- common/models.py
+++ common/models.py
@@ -1,3 +1,81 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import FileExtensionValidator, URLValidator
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+class Media(models.Model):
+    class MediaType(models.TextChoices):
+        IMAGE = "image", _("Image")
+        FILE = "file", _("File")
+        MUSIC = "music", _("Music")
+        VIDEO = "video", _("video")
+
+    file = models.FileField(_("File"), upload_to="files/", validators=[
+        FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png', 'gif', 'mp4', 'mp3', 'flac', 'doc', 'pdf'])], null=True, blank=True)
+    type = models.CharField(_("Type"), max_length=60, choices=MediaType.choices)
+
+    def __str__(self):
+        return str(self.id) + " " + str(self.file.name.split("/")[-1])
+
+    def clean(self):
+        if self.type == self.MediaType.IMAGE:
+            if not self.file.name.endswith(('.jpg', '.jpeg', '.png')):
+                raise ValidationError("File type is not image")
+        elif self.type == self.MediaType.FILE:
+            if not self.file.name.endswith(('.doc', '.pdf')):
+                raise ValidationError("File type is not document")
+        elif self.type == self.MediaType.MUSIC:
+            if not self.file.name.endswith(('.mp3', '.flac')):
+                raise ValidationError("File type is not music")
+        elif self.type == self.MediaType.VIDEO:
+            if not self.file.name.startwith('.mp4'):
+                raise ValidationError("File type is not video")
+        else:
+            raise ValidationError("File type is not valid")
+
+class Settings(models.Model):
+    home_image = models.ForeignKey(Media, on_delete=models.SET_NULL, null=True, blank=True)
+    home_title = models.CharField(_("Title"), max_length=120)
+    home_subtitle = models.CharField(_("Subtitle"), max_length=120)
+
+    def __str__(self):
+        return self.home_title
+
+
+class Country(models.Model):
+    name = models.CharField(_("Name"), max_length=120)
+    code = models.CharField(_("Code"), max_length=2)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _("Country")
+        verbose_name_plural = _("Countries")
+
+
+class Region(models.Model):
+    name = models.CharField(_("Name"), max_length=120)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name="regions")
+
+    def __str__(self):
+        return self.name
+
+
+class OurInstagramStory(models.Model):
+    image = models.ForeignKey(Media, on_delete=models.CASCADE, related_name="instagram_stories")
+    story_link = models.URLField(_("Story Link"), validators=[URLValidator()])  # TODO: WRITE VALIDATOR FOR INSTAGRAM URL
+
+    def __str__(self):
+        return f"Id: {self.id}|Link: {self.story_link}"
+
+
+class CustomerFeedback(models.Model):
+    description = models.TextField(_("Review"))
+    rank = models.IntegerField(_("Rank"))
+    customer_name = models.CharField(_("Customer Name"), max_length=80)
+    customer_position = models.CharField(_("Customer Position"), max_length=60)
+    customer_image = models.ForeignKey(Media, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.customer_name
--- products/models.py
+++ products/models.py
@@ -1,3 +1,94 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from common.models import Media
+from django_ckeditor_5.fields import CKEditor5Field
+
+from django.core.cache import cache
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(_("name"), max_length=255)
+    image = models.ForeignKey(Media, on_delete=models.SET_NULL, null=True, blank=True)
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _("Category")  # Bo'limlar
+        verbose_name_plural = _("Categories")
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+
+class Product(models.Model):
+    name = models.CharField(_("name"), max_length=255)
+    price = models.FloatField(_("price"))  # 2.03
+    short_description = models.TextField(_("short description"))
+    description = models.TextField(_("description"))
+    quanttity = models.IntegerField(_("quantity"))
+    instructions = CKEditor5Field(config_name='extends')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+    in_stock = models.BooleanField(_("in stock"), default=True)
+    brand = models.CharField(_("brand"), max_length=255)
+    discount = models.IntegerField(_("discount"), help_text=_("in percentage"))
+#     thumbnail = models.ForeignKey(Media, on_delete=models.SET_NULL, null=True, blank=True)
+
+#     objects = ProductManager()
+
+#     def __str__(self):
+#         return self.name
+
+#     def save(self, *args, **kwargs):
+#         cache.delete("all_products")
+#         self.category.save()
+
+
+class ProductColour(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="colours")
+    colour = models.ForeignKey(Media, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"Product: {self.product.id}|Colour: {self.colour.id}"
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="images")
+    image = models.ForeignKey(Media, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return f"Product: {self.product.id}|Image: {self.image.id}"
+
+
+class ProductSize(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="sizes")
+    value = models.CharField(_("value"), max_length=255)
+
+    def __str__(self):
+        return f"Product: {self.product.id}|Size: {self.value}"
+
+
+class ProductReview(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="reviews")
+    user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
+    title = models.CharField(_("title"), max_length=255)
+    review = models.TextField(_("review"))
+    rank = models.IntegerField(_("rank"), validators=[])
+    email = models.EmailField(_("email"))
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True)
+
+    def __str__(self):
+        return f"Product: {self.product.id}|User: {self.user.id}"
+
+    class Meta:
+        unique_together = ["product", "user"]
+
+
+class Wishlist(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="wishlists")
+    user = models.ForeignKey("accounts.User", on_delete=models.CASCADE, related_name="wishlists")
+
+    def __str__(self):
+        return f"Product: {self.product.id}|User: {self.user.id}"
